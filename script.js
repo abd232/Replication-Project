c6d13ea8ea817158a6fd124ba9d0c35886c27bfd
ahmadobeid98 <ahmad.a.obeid@gmail.com>
2026-01-07 06:47:23 +0200

    header almost done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var x = 1;
+const header = document.getElementById("siteHeader");
+const spacer = document.getElementById("headerSpacer");
+
+function syncHeader() {
+  const isCollapsed = (window.scrollY) > 0;
+  header.classList.toggle("is-collapsed", isCollapsed);
+  spacer.classList.toggle("is-collapsed", isCollapsed);
+}
+
+window.addEventListener("scroll", syncHeader, { passive: true });
+window.addEventListener("resize", syncHeader);
+syncHeader();
